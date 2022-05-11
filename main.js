55fa099b64d71fda3582a2c8b05926f70f2af2e0
Juan Toro <jct20xx@gmail.com>
2022-05-11 10:55:00 -0400

    commit tree 5627ba0bafaad69468073177168c8e976bd0f7e4

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+let likes = [...document.getElementsByClassName('like-glyph')]
+let errorBanner = document.getElementById('modal')
+let errorContent = document.getElementById('modal-message')
 
 // Your JavaScript code goes here!
+   
+let serverCallandReact = (event) => {
+  mimicServerCall()
+  .then(() =>hearts(event))
+  .catch((error) => errorAlert(error))
+}
 
+let hearts = (event) => {
+  if (event.target.textContent === EMPTY_HEART) {
+    event.target.classList.add('activated-heart')
+    event.target.textContent = FULL_HEART
+  }
+  else {
+    event.target.classList.remove('activated-heart')
+    event.target.textContent = EMPTY_HEART
+  }
+}
 
+let errorAlert= (error) => {
+  errorBanner.classList.remove('hidden')
+  errorContent.innerText = error
+  setTimeout(() =>{
+    errorBanner.classList.add('hidden')
+  }, 3000) 
+}
+
+likes.map(heart => heart.addEventListener('click', serverCallandReact))
 
 
 //------------------------------------------------------------------------------
